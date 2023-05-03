8cc97aa53e23e5fa952a6543cd5bee6fe5c52149
J-Skarbek <john@snarkmediaagency.com>
2023-05-03 18:15:53 -0500

    Create a rough header and upload a hamburger icon svg

---
+++ shopping-cart-app/src/components/Header.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import reactLogo from './assets/react.svg';
+import hamburger from './assets/hamburger.svg';
+
+function Header() {
+
+  return (
+    <div className="header flex">
+      <div className="logo">
+        <img className="logo-img" src={reactLogo} alt="logo" />
+      </div>
+      <div className="nav-menu flex">
+        <a href="#">Home</a>
+        <a href="#">Shop</a>
+        <a href="#">Cart</a>
+      </div>
+      <div className="cart-hamburger-icon">
+        <img src={hamburger} className="hamburger" alt="View Your Cart" />
+      </div>
+    </div>
+  )
+}
+
+export default Header;
