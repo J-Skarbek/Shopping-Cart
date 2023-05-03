02d7f677d508c705b50f5fc975e8801eea347655
J-Skarbek <john@snarkmediaagency.com>
2023-05-03 18:56:14 -0500

    Create a basic email signup form

--- shopping-cart-app/src/components/MailingListSignUp.jsx
+++ shopping-cart-app/src/components/MailingListSignUp.jsx
@@ -4,7 +4,14 @@ function MailingListSignUp() {
 
   return (
     <div className="mailing-list-section flex">
-
+      <form>
+        <input type="text" id="first-name" name="first_name">First Name</input>
+        <label htmlFor="first-name"></label>
+        <input type="text" id="last-name" name="last_name">First Name</input>
+        <label htmlFor="last-name"></label>
+        <input type="email" id="email" name="email">First Name</input>
+        <label htmlFor="email"></label>
+      </form>
     </div>
   )
 }
