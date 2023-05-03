5fcf563c02913e55562008ee9fd7c1fd3c75cca5
J-Skarbek <john@snarkmediaagency.com>
2023-05-03 18:39:11 -0500

    Move hero section elements to their own component and import them into the homepage

--- shopping-cart-app/src/components/Homepage.jsx
+++ shopping-cart-app/src/components/Homepage.jsx
@@ -1,24 +1,11 @@
 import React from "react";
+import HeroSection from "./HeroSection";
 
 function Homepage() {
 
   return (
     <div className="homepage-body">
-      <div className="hero-section">
-        <div className="hero-text left-side">
-          <h1>Main Page Heading</h1>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
-            nisi ut aliquip ex ea commodo consequat.
-          </p>
-          <div className="buttons">
-            <button type="button">Shop Now</button>
-            <button type="button">Join Our Mailing List</button>
-          </div>
-        </div>  
-      </div>
+      <HeroSection />
     </div>
   )
 }
