44deaac927cba772c205be1b9ea072d651c89ae4
J-Skarbek <john@snarkmediaagency.com>
2023-05-03 19:16:03 -0500

    Start on product card componenet

--- shopping-cart-app/src/App.jsx
+++ shopping-cart-app/src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './components/Header';
-import MailingListSignUp from './components/MailingListSignUp';
+// import MailingListSignUp from './components/MailingListSignUp';
 
 function App() {
   const [count, setCount] = useState(0)
