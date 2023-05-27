ddae3030ba954dcc32e350cb58410c9d99456c53
J-Skarbek <john@snarkmediaagency.com>
2023-05-27 15:24:45 -0500

    Create a button component

---
+++ shopping-cart-app/src/components/Button.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Button(props) {
+
+  return (
+    <div className="button">
+      <button type="button"></button>
+    </div>
+  )
+}
+
+export default Button;
