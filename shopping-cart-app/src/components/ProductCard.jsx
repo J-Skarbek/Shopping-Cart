c1bf3906327d02d9cf6fdb8b49f157749d1be01a
J-Skarbek <john@snarkmediaagency.com>
2023-05-27 14:00:57 -0500

    Get data to pull in correctly on the cards for any given category of product

--- shopping-cart-app/src/components/ProductCard.jsx
+++ shopping-cart-app/src/components/ProductCard.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import ProductData from '../productData.json';
 
-function ProductCard() {
+function ProductCard(props) {
 
   return (
-    <div className="product-card bg-red-400">
-      {/* <img src={ProductData.ProductData.Tops.products[0].images.image} /> */}
-      <img src={ProductData.Tops.products[0].images.image} alt={ProductData.Tops.products[0].name} />
-      <p>testing</p>
+    <div className="product-card bg-stone-200 w-80 m-4 rounded-2xl">
+      <div className="featured-img">
+        <img className="rounded-t-2xl" src={props.img} alt={props.name} />
+      </div>
+      <div className="product-details p-4">
+        <p className="product-name font-semibold text-lg">{props.name}</p>
+        <p className="product-price">{ProductData.Tops.products[0].price}</p>
+      </div>
     </div>
   )
 }
--- shopping-cart-app/src/components/ProductsOverview.jsx
+++ shopping-cart-app/src/components/ProductsOverview.jsx
@@ -4,17 +4,25 @@ import ProductData from '../productData.json';
 
 function ProductsOverview() {
 
+  const [products, setProducts] = React.useState(ProductData);
+
+  console.log(products)
+  console.log(products.Tops.products)
+
+
+  
+  const prodCards = products.Tops.products.map(top => {
+    return (
+      <ProductCard 
+        name={top.name}
+        img={top.images.image}
+      />
+    )
+  });
+
   return (
-    <div className='card-grid my-0 mx-auto w-4/5 flex flex-wrap justify-around items-center '>
-      <ProductCard /> 
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
+    <div className='card-grid my-0 mx-auto w-4/5 flex flex-wrap justify-around items-center'>
+      { prodCards }
     </div>
   )
 }
