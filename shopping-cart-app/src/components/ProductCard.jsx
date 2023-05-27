08f86ae20fb1db0b3ab11627dd5a918b8381d37c
J-Skarbek <john@snarkmediaagency.com>
2023-05-27 12:52:10 -0500

    Work on getting card component stylized

--- shopping-cart-app/src/components/ProductCard.jsx
+++ shopping-cart-app/src/components/ProductCard.jsx
@@ -5,6 +5,8 @@ function ProductCard() {
 
   return (
     <div className="product-card bg-red-400">
+      {/* <img src={ProductData.ProductData.Tops.products[0].images.image} /> */}
+      <img src={ProductData.Tops.products[0].images.image} alt={ProductData.Tops.products[0].name} />
       <p>testing</p>
     </div>
   )
--- shopping-cart-app/src/components/ProductsOverview.jsx
+++ shopping-cart-app/src/components/ProductsOverview.jsx
@@ -5,8 +5,8 @@ import ProductData from '../productData.json';
 function ProductsOverview() {
 
   return (
-    <div className='card-grid'>
-      <ProductCard />
+    <div className='card-grid my-0 mx-auto w-4/5 flex flex-wrap justify-around items-center '>
+      <ProductCard /> 
       <ProductCard />
       <ProductCard />
       <ProductCard />
