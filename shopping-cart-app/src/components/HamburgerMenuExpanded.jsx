1260bd3d7af47735f5a5def1603c7d5049b7824b
J-Skarbek <john@snarkmediaagency.com>
2023-06-12 17:05:48 -0500

    Start on styling the hambuger expended menu

--- shopping-cart-app/src/components/HamburgerMenuExpanded.jsx
+++ shopping-cart-app/src/components/HamburgerMenuExpanded.jsx
@@ -3,29 +3,29 @@ import React from "react";
 function HamburgerMenuExpanded(props) {
 
   return (
-    <div className="hamburger-open flex flex-col">
-      <div className="products-in-cart">
+    <div className="hamburger-open flex flex-col bg-slate-200 w-96 p-4">
+      <div className="products-in-cart mb-4">
         <p>Therre are currently no products in your cart.</p>
       </div>
       <div className="order-details">
-        <div className="order-value flex">
+        <div className="order-value flex justify-between">
           <p>Order Value</p>
-          <p>$99.99 Sample Value</p>
+          <p>$99.99(ex)</p>
         </div>
-        <div className="order-value flex">
+        {/* <div className="order-value flex justify-between mb-4">
           <p>Order Value</p>
-          <p>$99.99 Sample Value</p>
-        </div>
-        <div className="shipping-value flex">
+          <p>$99.99(ex)</p>
+        </div> */}
+        <div className="shipping-value flex justify-between mb-4">
           <p>Shipping</p>
-          <p>FREE</p>
+          <p className="uppercase">FREE</p>
         </div>
-        <div className="order-total flex">
+        <div className="order-total flex justify-between mb-4">
           <p>Total</p>
           <p>$99.99</p>
         </div>
       </div>
-      <div className="buttons-container">
+      <div className="buttons-container flex flex-col">
         <button type="button">Checkout</button>
         <button type="Shopping Cart">Cart</button>
       </div>
