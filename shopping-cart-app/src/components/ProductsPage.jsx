3f92e3dfbcdb00e0ca4640838ce950d3c59e5ee7
J-Skarbek <john@snarkmediaagency.com>
2023-06-12 15:04:19 -0500

    Create filler text on the shopping cart and products/shop page componenets

---
+++ shopping-cart-app/src/components/ProductsPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function ProductsPage() {
+
+  return (
+    <div>
+      <p>This is the shopping/products page.</p>
+    </div>
+  )
+}
+
+export default ProductsPage;
---
+++ shopping-cart-app/src/components/ShoppingCart.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ShoppingCart() {
+  return (
+    <div>
+      <p>This will be the shopping cart page.</p>
+    </div>
+  )
+}
+
+export default ShoppingCart;
