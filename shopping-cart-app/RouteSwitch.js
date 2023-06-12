f2ac6d595cd1220a0eb67caae8dd0d49de360a99
J-Skarbek <john@snarkmediaagency.com>
2023-06-12 15:13:19 -0500

    Create RouteSwitch file

---
+++ shopping-cart-app/RouteSwitch.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Homepage from './src/components/Homepage';
+import ShoppingCart from "./src/components/ShoppingCart";
+import ProductsOverview from "./src/components/ProductsOverview";
+
+const RouteSwitch = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Homepage />} />
+        <Route path='/shop' element={<ProductsOverview />} />
+        <Route path='/cart' element={<ShoppingCart />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default RouteSwitch;
