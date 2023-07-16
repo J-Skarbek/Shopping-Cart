2f05aa18bf351210a1bcebe76af6e3bddbd67608
J-Skarbek <john@snarkmediaagency.com>
2023-07-16 14:01:23 -0500

    Add a basic single product page

---
+++ shopping-cart-app/src/components/Product.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function ProductPage(props) {
+
+  return (
+    <div>
+      <p>This is the product page for {props.name}</p>
+    </div>
+  )
+}
+
+export default ProductPage;
