729b90eb5e6962a2cf9501e4656ffec02ebcf97c
J-Skarbek <john@snarkmediaagency.com>
2023-07-25 17:49:20 -0500

    Finally able to pull in and render a component using data from the json api

--- shopping-cart-app/src/components/Product.jsx
+++ shopping-cart-app/src/components/Product.jsx
@@ -5,33 +5,59 @@ function Product(props) {
 
   const { id } = useParams();
   const productDetails = useLoaderData();
+  const { products } = productDetails;
   // console.log(productDetails.products[0]);
   // console.log(id)
   // console.log(productDetails.products)
 
  function getAllProductDetails() {
-  productDetails.products.forEach(detail => {
+  let indiviudualDetails;
+  products.map(detail => {
     if (id === detail.name) {
       console.log(detail.desciption)
+      console.log(detail.images.image)
+      indiviudualDetails = detail;
+      console.log(indiviudualDetails.name);
     }
   })
-}
-
-getAllProductDetails();
-
   return (
     <div>
       <div className="details">
         <h2>{ id }</h2>
       </div>
-      <p>This is the product page for {productDetails.products[0].name}</p>
+      <p>This is the product page for {indiviudualDetails.name}</p>
       <p>Testing Product page</p>
       <div className="more-detail">
-        <h2>Product Details</h2>
-        <p>{productDetails.products[0].desciption}</p>
-        {/* <img src={productDetails.products[0].images.image2}/> */}
+        <h2>Product Details22</h2>
+        <p>{indiviudualDetails.desciption}</p>
+        <img src={indiviudualDetails.images.image2}></img>
       </div>
     </div>
+  )
+}
+
+// getAllProductDetails();
+
+  return (
+    <div>
+      {
+      getAllProductDetails()
+      }
+    </div>
+
+   
+    // <div>
+    //   <div className="details">
+    //     <h2>{ id }</h2>
+    //   </div>
+    //   <p>This is the product page for {productDetails.products[0].name}</p>
+    //   <p>Testing Product page</p>
+    //   <div className="more-detail">
+    //     <h2>Product Details</h2>
+    //     <p>{productDetails.products[1].desciption}</p>
+    //     <img src={productDetails.products[0].images.image2}></img>
+    //   </div>
+    // </div>
   )
 }
 
