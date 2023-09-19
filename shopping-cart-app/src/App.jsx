16090994cd46be885b54280302534f79efa657f9
J-Skarbek <john@snarkmediaagency.com>
2023-09-19 17:09:56 -0500

    Clean up the app component, remove an old router switch and test out ScrollRestoration

--- shopping-cart-app/src/App.jsx
+++ shopping-cart-app/src/App.jsx
@@ -1,16 +1,13 @@
 import React from "react";
-import { createBrowserRouter, Route, Link, NavLink, createRoutesFromElements, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, Route, Link, NavLink, createRoutesFromElements, RouterProvider, ScrollRestoration } from "react-router-dom";
+import ScrollToTop from "./components/ScrollToTop";
 import { topsLoader } from "./components/ShopPage";
 import './App.css'
-import Header from './components/Header';
 import Homepage from './components/Homepage';
-import Footer from './components/Footer';
 import ShoppingCart from "./components/ShoppingCart";
 import ProductsOverview from "./components/ShopPage";
 import RootLayout from "./components/RootLayout";
 import Product, { productDetailsLoader } from "./components/Product";
-import ProductCard from "./components/ProductCard";
-// import Root from '../RouteSwitch';
 // import MailingListSignUp from './components/MailingListSignUp';
 
 
@@ -39,9 +36,10 @@ function App() {
   const [cartContents, setCartContents] = React.useState([1,2,3,4,5]);
 
   return (
-    <RouterProvider
-      router={router}
-    />
+    <RouterProvider router={router} >
+      <ScrollRestoration />
+    </RouterProvider>
+
   )
 }
 
