de21badc91b20d280b61514a6dcd04f74dce0597
J-Skarbek <john@snarkmediaagency.com>
2023-10-02 10:12:30 -0500

    Comment out/delete lines not being used anymore

--- shopping-cart-app/src/components/ShopPage.jsx
+++ shopping-cart-app/src/components/ShopPage.jsx
@@ -7,15 +7,15 @@ import Pill from './Pill';
 
 function ProductsOverview(props) {
 
-  const { id } = useParams();
+  // const { id } = useParams();
 
   const tops = useLoaderData();
 
-  const [products, setProducts] = React.useState(ProductData);
+  // const [products, setProducts] = React.useState(ProductData);
 
-  const { Tops, Shoes, Dresses, Skirts, Accessories } = products;
+  // const { Tops, Shoes, Dresses, Skirts, Accessories } = products;
 
-  console.log(tops);
+  // console.log(tops);
 
   // const displayAllProducts = () => {
   //   const newArray = [];
@@ -51,58 +51,58 @@ function ProductsOverview(props) {
   //   return all;
   // }
 
-  const changeCategory = e => {
-    console.log(e.target.innerText);
-    if (e.target.innerText === 'Tops') {
-      const topsArray = [];
-      Tops.products.forEach(item => {
-        topsArray.push(item);
-      });
-      console.log(topsArray)
-      let topsOnly = topsArray.map(product => {
-        return (
-          <ProductCard 
-            name={product.name}
-            description={product.description}
-            img={product.images.image}
-            price={product.price}
-            key={nanoid()}
-          />
-        );
-      });
-      return topsOnly;
-    }
-  }
+  // const changeCategory = e => {
+  //   console.log(e.target.innerText);
+  //   if (e.target.innerText === 'Tops') {
+  //     const topsArray = [];
+  //     Tops.products.forEach(item => {
+  //       topsArray.push(item);
+  //     });
+  //     console.log(topsArray)
+  //     let topsOnly = topsArray.map(product => {
+  //       return (
+  //         <ProductCard 
+  //           name={product.name}
+  //           description={product.description}
+  //           img={product.images.image}
+  //           price={product.price}
+  //           key={nanoid()}
+  //         />
+  //       );
+  //     });
+  //     return topsOnly;
+  //   }
+  // }
 
-  const getProductCategories = () => {
-    let productCategories = [];
-    for (let property in products) {
-      // console.log(`${property}`);
-      productCategories.push(property);
-    }
-    let cats = productCategories.map(category => {
-      return (
-        <Pill 
-          categoryName={category}
-          handleClick={changeCategory}
-          key={nanoid()}
-        />
-      )
-    })
-    return cats;
-  }
+  // const getProductCategories = () => {
+  //   let productCategories = [];
+  //   for (let property in products) {
+  //     // console.log(`${property}`);
+  //     productCategories.push(property);
+  //   }
+  //   let cats = productCategories.map(category => {
+  //     return (
+  //       <Pill 
+  //         categoryName={category}
+  //         handleClick={changeCategory}
+  //         key={nanoid()}
+  //       />
+  //     )
+  //   })
+  //   return cats;
+  // }
 
-  let displayArray = [];
-  const allProds = tops.map(category => {
-    displayArray.push(category)
-  });
-  console.log(displayArray)
+  // let displayArray = [];
+  // const allProds = tops.map(category => {
+  //   displayArray.push(category)
+  // });
+  // console.log(displayArray)
 
   return (
     <div className='products-container'>
       <div className='category-pills flex justify-center gap-2 my-8'>
         <button type='button'>Get all Products</button>
-          { getProductCategories() }
+          {/* { getProductCategories } */}
         </div>
       <div className='card-grid my-0 mx-auto w-4/5 flex flex-wrap justify-around items-center'>
         {tops.map(category => {
@@ -134,8 +134,3 @@ export const topsLoader = async () => {
   // const res = await fetch('http://localhost:4000/Tops/')
 }
 
-// export const productDetailsLoader = async ({params}) => {
-//   const { id } = params;
-//   const res = await fetch('http://localhost:4000/Tops/' + id);
-//   return res.json();
-// }
