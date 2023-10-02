856d43a242532d74c267033574e912cf384b23ba
J-Skarbek <john@snarkmediaagency.com>
2023-10-02 11:45:04 -0500

    Restyle the pill buttons

--- shopping-cart-app/src/components/Pill.jsx
+++ shopping-cart-app/src/components/Pill.jsx
@@ -2,22 +2,22 @@ import React from "react";
 
 function Pill(props) {
 
-  // const handleClick = () => {
-  //   console.log('I am clicked');
-  //   console.table(document.querySelectorAll('.pill').textContent)
-  // }
+  const handleClick = () => {
+    console.log('I am clicked');
+  }
 
   //if pill is clicked, look at the textContent in the pill, then
   //Find the category in productData, then 
   //map over the products in that category
 
   return (
-    <div 
-      className="pill bg-fuchsia-100 py-1 px-6 rounded-3xl drop-shadow-lg" 
-      onClick={props.handleClick}
-    >
-      <p>{props.categoryName}</p>
-    </div>
+    <>
+      <button 
+        type="button" 
+        className="pill bg-fuchsia-100 py-1 px-6 rounded-3xl drop-shadow-lg" 
+        onClick={handleClick}>{props.categoryName}
+      </button>
+    </>
   )
 }
 
