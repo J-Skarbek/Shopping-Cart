f37a7cbe4d422059b7e0045e8afaf241c73f9b85
J-Skarbek <john@snarkmediaagency.com>
2023-11-24 18:33:37 -0600

    Get searchParams working on the categories properly

--- shopping-cart-app/src/components/ShopPage.jsx
+++ shopping-cart-app/src/components/ShopPage.jsx
@@ -6,7 +6,6 @@ import GetAllProducts from './GetAllProductCards';
 import Pill from './Pill';
 
 export const productsLoader = async () => {
-  // const { id } = params;
   const res = await fetch('http://localhost:4000/allProudcts')
   return res.json();
 }
@@ -17,18 +16,32 @@ export default function ProductsOverview(props) {
   const [searchParams, setSearchParams] = useSearchParams();
   const categoryFilter = searchParams.get("category");
   const [products, setProducts] = React.useState(productArray);
-  console.log(searchParams.toString())
-  console.log(`category is ${categoryFilter}`)
-  console.log(productArray[0].category);
+
+  function getAllProducts() {
+    const allProducts = [];
+    const allCategories = productArray.map(category => {
+      const products = category.products.map(product => {
+        allProducts.push(product);
+      });
+    });
+    console.log(allProducts)
+    return allProducts
+  }
 
   const filterProducts = categoryFilter
-    ? productArray.filter(prod => prod.category.toLowerCase() === categoryFilter)
-    : productArray
+    ? productArray.filter(prod => prod.category.toLowerCase() === categoryFilter)[0].products
+    : getAllProducts()
   ;
 
-  console.log(filterProducts)
-
-  
+  const displayProducts = filterProducts.map(product => (
+    <ProductCard 
+      name={product.name}
+      description={product.desciption}
+      img={product.imageArray[0]}
+      price={product.price}
+      key={nanoid()}
+    />
+  ))
 
   const displayCategories = products.map(category => {
     return (
@@ -61,13 +74,6 @@ export default function ProductsOverview(props) {
     return displayProductsGrid;
   }
 
-  //Need to find a way to repurpose the displayProductsGrid to be more dynamic --->
-  //Specfically, need it to, by default, load all products, but also, be able to function with the category selection buttons
-  //in the pill component and re-render the grid if someone wants to filter by product type
-  //It should be almost there, but the problem seems to be that when someone clicks on a category button, it triggers an error
-  //Because the structure of the new array being assigned to the state value is slightly different in structure than the
-  //original JSON file's structure
-
   return (
     <div className='products-container'>
       <div className='category-pills flex justify-center gap-2 my-8'>
@@ -85,21 +91,7 @@ export default function ProductsOverview(props) {
           { displayCategories }
         </div>
       <div className='card-grid my-0 mx-auto w-4/5 flex flex-wrap justify-around items-center'>
-        {/* {products.map(category => {
-          let displayProducts = category.products.map(product => {
-            return (
-              <ProductCard 
-                name={product.name}
-                description={product.desciption}
-                img={product.images.image}
-                price={product.price}
-                key={nanoid()}
-              />
-            )
-          })
-          return displayProducts;
-        })} */}
-        { displayProductsGrid }
+        { displayProducts }
       </div>
     </div>
   )
