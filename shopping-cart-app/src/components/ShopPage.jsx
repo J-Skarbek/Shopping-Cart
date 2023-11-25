5424c9a9b98de79c9352c5a38571c3e6ec648f46
J-Skarbek <john@snarkmediaagency.com>
2023-11-25 12:15:21 -0600

    Get query params working for filtering products and remove old code

--- shopping-cart-app/src/components/ShopPage.jsx
+++ shopping-cart-app/src/components/ShopPage.jsx
@@ -16,6 +16,7 @@ export default function ProductsOverview(props) {
   const [searchParams, setSearchParams] = useSearchParams();
   const categoryFilter = searchParams.get("category");
   const [products, setProducts] = React.useState(productArray);
+  console.log(productArray)
 
   function getAllProducts() {
     const allProducts = [];
@@ -27,10 +28,19 @@ export default function ProductsOverview(props) {
     return allProducts;
   }
 
-  const filterProducts = categoryFilter
-    ? productArray.filter(prod => prod.category.toLowerCase() === categoryFilter)[0].products
-    : getAllProducts()
-  ;
+  const dipslayCategoryFilters = productArray.map(category => {
+    return (
+      <Link 
+        to={`?category=${category.category}`}
+        key={nanoid()}
+      >{`${category.category}`}
+      </Link>
+    )
+  })
+
+  const filterProducts = categoryFilter ? 
+    productArray.filter(prod => prod.category.toLowerCase() === categoryFilter)[0].products
+    : getAllProducts();
 
   const displayProducts = filterProducts.map(product => (
     <ProductCard 
@@ -42,52 +52,17 @@ export default function ProductsOverview(props) {
     />
   ))
 
-  const displayCategories = products.map(category => {
-    return (
-      <Pill
-        categoryName={category.category}
-        key={nanoid()}
-        displayedProducts={products}
-        updateDisplayedProducts={setProducts}
-      />
-    )
-  })
-
-  const displayProductsGrid = products.map(category => {
-    let displayCards = category.products.map(individualProduct => {
-      return (
-        <ProductCard 
-          name={individualProduct.name}
-          description={individualProduct.desciption}
-          img={individualProduct.imageArray[0]}
-          price={individualProduct.price}
-          key={nanoid()}
-        />
-      )
-    })
-    return displayCards;
-  })
-
-  function resetProductGrid() {
-    console.log('clicked!');
-    return displayProductsGrid;
-  }
-
   return (
     <div className='products-container'>
       <div className='category-pills flex justify-center gap-2 my-8'>
         <button 
           type='button' 
           className="pill bg-fuchsia-100 py-1 px-6 rounded-3xl drop-shadow-lg" 
-          onClick={resetProductGrid}
+          // onClick={resetProductGrid}
         >
           All Products
         </button>
-          <GetAllProducts 
-            allProducts={products}
-            updateDisplayedProducts={setProducts}
-          />
-          { displayCategories }
+          { dipslayCategoryFilters }
         </div>
       <div className='card-grid my-0 mx-auto w-4/5 flex flex-wrap justify-around items-center'>
         { displayProducts }
