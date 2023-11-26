81058602f4cc981d6362428972d5d036b8848476
J-Skarbek <john@snarkmediaagency.com>
2023-11-26 11:43:57 -0600

    Get error message to display on the error component render

--- shopping-cart-app/src/components/Error.jsx
+++ shopping-cart-app/src/components/Error.jsx
@@ -1,12 +1,13 @@
 import React from "react";
-// import { useRouteError } from "react-router-dom";
+import { useRouteError } from "react-router-dom";
 
 export default function Error() {
-  // const error = useRouteError();
-  // typeof(error)
+  const error = useRouteError();
+  
   return (
-    <>
+    <div className="flex flex-col">
       <h1>There was an error here.</h1>
-    </>
+      <p>{error.message}</p>
+    </div>
   )
 }
