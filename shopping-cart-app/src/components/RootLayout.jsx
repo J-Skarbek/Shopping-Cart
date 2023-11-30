2f175a16e7e702961d7dc10070548b6c7d07cc3a
J-Skarbek <john@snarkmediaagency.com>
2023-11-30 10:15:14 -0600

    Start on adding context provider

--- shopping-cart-app/src/components/RootLayout.jsx
+++ shopping-cart-app/src/components/RootLayout.jsx
@@ -1,28 +1,50 @@
-import React from "react";
+import React, { createContext } from "react";
 import { Outlet, ScrollRestoration } from "react-router-dom";
 import Header from "./Header";
 import Footer from "./Footer";
+
+export const ShopContext = createContext({
+  products: [],
+  cartItems: [],
+  addToCart: () => {},
+  logCartItems: () => {},
+  emptyTheCart: () => {}
+})
 
 function RootLayout() {
 
   const [cartContents, setCartContents] = React.useState([{}, {}, 3, 4, 5, 6, {}]);
   // console.log(cartContents);
 
+  const cartItems = cartContents;
+
+  function addToCart(newItem) {
+    setCartContents(prevCartContents => {
+      return ({
+      ...prevCartContents,
+      newItem
+      })
+    })
+    console.log(`updated cart contains: ${console.table(cartContents)}`);
+  }
+
   return (
-    <div className="root-layout">
-      <Header 
-        cartContents={cartContents}
-        setCartContents={setCartContents}
-      />
-      <main className="flex justify-center">
-        <Outlet 
+    <ShopContext.Provider value={{ cartItems, addToCart }}>
+      <div className="root-layout">
+        <Header 
           cartContents={cartContents}
           setCartContents={setCartContents}
         />
-      </main>
-      <Footer />
-      <ScrollRestoration />
-    </div>
+        <main className="flex justify-center">
+          <Outlet 
+            cartContents={cartContents}
+            setCartContents={setCartContents}
+          />
+        </main>
+        <Footer />
+        <ScrollRestoration />
+      </div>
+    </ShopContext.Provider>
   )
 }
 
