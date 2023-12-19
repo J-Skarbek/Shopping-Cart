910e6daad3c93697fb0a00b5100f94561a752097
J-Skarbek <john@snarkmediaagency.com>
2023-12-19 11:56:20 -0600

    Add a little more text

--- shopping-cart-app/src/components/ShoppingCartCard.jsx
+++ shopping-cart-app/src/components/ShoppingCartCard.jsx
@@ -6,9 +6,9 @@ export default function ShoppingCartCard({image, productName, price, size, quant
     <div className="flex">
         <img src={image} className="w-1/6 mr-4 mb-4"></img>
         <div className="prod-overview flex flex-col px-4">
-          <p>{productName}</p>
-          <p>{price}</p>
-          <p>Quantity will go here.</p>
+          <p>Product: {productName}</p>
+          <p>Price: {price}</p>
+          <p>Quantity: Quantity will go here.</p>
         </div>
     </div>
   )
