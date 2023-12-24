ab566d870125731132f5832f79de11356c307633
J-Skarbek <john@snarkmediaagency.com>
2023-12-24 11:48:24 -0600

    Update/clenup some of the homepage and homepage hero components

--- shopping-cart-app/src/components/HeroSection.jsx
+++ shopping-cart-app/src/components/HeroSection.jsx
@@ -1,28 +1,9 @@
 import React from "react";
-import ProductData from '../productData.json';
-import '../App.css';
 
-function HeroSection(props) {
-
-  // const styles = {
-  //   backgroundImage: {
-  //     url(props.backgroundImage)
-  //   },
-  // }
-
-
-  function dataCallClick() {
-    console.log(ProductData.Tops.products[0].name)
-    console.log(ProductData.Tops.products[0].sizes.xs)
-    console.log(ProductData.Tops.products[0].sizes)
-    console.log(ProductData.Tops.products[0].desciption)
-  }
-
-  // console.log(props.backgroundImage)
+function HeroSection() {
 
   return (
-    <div className="hero-section h-[75vh] min-h-[90vh] max-2xl:min-h-[75vh] bg-cover bg-no-repeat bg-top text-white p-12">
-    {/* <div className="hero-section" style={styles.backgroundImage}> */}
+    <div className="hero-section h-[75vh] min-h-[90vh] max-2xl:min-h-[75vh] bg-[url('../src/assets/editorialPhotos/homepage-hero.jpg')] bg-cover bg-no-repeat bg-top bg-blend-overlay text-white p-12">
         <div className="hero-text left-side flex flex-col justify-center h-full">
           <h1 className="font-bold pb-8">Main Page Heading</h1>
           <p className="pr-[50%] pb-72">
@@ -34,7 +15,6 @@ function HeroSection(props) {
           <div className="buttons">
             <button type="button">Shop Now</button>
             <button type="button">Join Our Mailing List</button>
-            <button type="button" onClick={dataCallClick}>Test call the data</button>
           </div>
         </div>  
       </div>
--- shopping-cart-app/src/components/Homepage.jsx
+++ shopping-cart-app/src/components/Homepage.jsx
@@ -1,16 +1,11 @@
 import React from "react";
 import HeroSection from "./HeroSection";
-import ProductsOverview from "./ShopPage";
-import homepageHero from "../assets/editorialPhotos/homepage-hero.jpg"
 
 function Homepage() {
 
   return (
     <div className="homepage-body w-full">
-      <HeroSection 
-        backgroundImage={homepageHero}
-      />
-      {/* <ProductsOverview /> */}
+      <HeroSection />
     </div>
   )
 }
