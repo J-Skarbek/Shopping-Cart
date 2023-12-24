9f61caf1ddeabbb854020eda9903246bcfad8ccc
J-Skarbek <john@snarkmediaagency.com>
2023-12-24 13:31:46 -0600

    Working on method to add to cart and adjust quantity if add to cart button is clicked

--- shopping-cart-app/src/components/RootLayout.jsx
+++ shopping-cart-app/src/components/RootLayout.jsx
@@ -38,22 +38,66 @@ function RootLayout() {
   //   return test;
   // }
 
+  // function testCartAdd(newItem) {
+  //   console.log(newItem.id);
+  //   cartContents.length === 0 ? setCartContents(newItem) : 
+  //   cartContents.map(item => {
+  //     // item.id == newItem.id ? console.log(`Matched product ID:${newItem.id} with ${newItem.name}`) : console.log('could not match product IDs');
+  //     if (item.id == newItem.id) {
+  //       item.quantityInCart += 1;
+  //     } else {
+  //       setCartContents(prevCartContents => {
+  //         console.log('this is spreading in the cart info')
+  //         return ([
+  //           ...prevCartContents,
+  //           newItem
+  //         ])
+  //       })
+  //     }
+  //   })
+  // }
+
   function testCartAdd(newItem) {
-    console.log(newItem.id);
-    cartContents.map(item => {
-      // item.id == newItem.id ? console.log(`Matched product ID:${newItem.id} with ${newItem.name}`) : console.log('could not match product IDs');
-      if (item.id === newItem.id) {
-        item.quantityInCart += 1;
-      } else {
-        setCartContents(prevCartContents => {
-          return ([
-            ...prevCartContents,
-            newItem
-          ])
-        })
-      }
+    if (cartContents.length === 0) {
+      setCartContents(newItem)
+      console.log(newItem.id)
+
+    }
+    const updated = cartContents.map(item => {
+      item.id == newItem.id
+      ? { ...item, quantityInCart: item.quantityInCart += 1 }
+      : item
     })
-  } 
+    console.log('array had items, updated to:')
+    setCartContents([updated]);
+    
+  }
+
+  //actual fn name is addItemToCart()
+  // function testCartAdd(newItem) {
+  //   console.log(`Adding new item: ${newItem}`);
+  //   if (cartContents.length === 0) {
+  //     setCartContents([newItem]);
+  //     console.log(cartContents)
+  //   } else {
+  //     console.log('else triggered')
+      // const updatedProducts = cartContents.map(item => {
+      //   if (item.id == newItem.id) {
+      //     return item.quantityInCart += 1;
+      //   }
+      // })
+      // setCartContents([
+      //     ...updatedProducts,
+      //     newItem
+      //   ]);
+    //   }
+    // }
+  
+  
+  // function filterAndAddProduct(newItem) {
+  //   console.log(newItem.id);
+  //   cartContents.filter((item => newItem.id == item.id))
+  // }
     
   function addToCart(newItem) {
     setCartContents(prevCartContents => {
@@ -64,6 +108,14 @@ function RootLayout() {
     })
     console.log(`updated cart contains: ${newItem}`);
   }
+
+  // function increaseQuntity() {
+  //   //item.id -= 1;
+  // }
+
+  // function decraseQuantity() {
+  //   //item.id += 1;
+  // }
   
   // Will eventually need to add removeFromCart function, possibly only need 
   // on the actualy cart page
