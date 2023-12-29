24a315a603d4800ace26de727a7a2d5d7d36c3ee
J-Skarbek <john@snarkmediaagency.com>
2023-12-29 15:18:06 -0600

    Loop over the sizes in a products size array to generate select options in the form

--- shopping-cart-app/src/components/Product.jsx
+++ shopping-cart-app/src/components/Product.jsx
@@ -106,11 +106,14 @@ export default function Product() {
               <form>
                 <input type="number" min="0" name="quantity" id="quantity" className="form-input" />
                 <select name="size-select" id="size-select">
-                  <option value="xs">XS</option>
-                  <option value="s">SM</option>
-                  <option value="m">M</option>
-                  <option value="l">L</option>
-                  <option value="xl">XL</option>
+                  { details.sizes.map(sizeValue => {
+                    return (
+                      // Normally wouln't use the value returned from array as the key
+                      // but the size values options on each won't be changing, so it should be fine
+                      // in this use case
+                      <option value={sizeValue} key={sizeValue}>{sizeValue.toUpperCase()}</option>
+                    )
+                  })}
                 </select>
                 <button type="button" onClick={() => testCartAdd(details)}>Add to Cart</button>
               </form>
