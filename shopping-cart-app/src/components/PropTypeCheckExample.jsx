7f77cf4a736fafdb1305dc1bac0b2a645f130f94
J-Skarbek <john@snarkmediaagency.com>
2024-02-01 18:06:25 -0600

    Basic proptype example

---
+++ shopping-cart-app/src/components/PropTypeCheckExample.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const RenderName = (props) => {
+  return (
+    <div>{props.name}</div>
+  );
+}
+
+RenderName.propTypes = {
+  name: PropTypes.string
+};
+
+export default RenderName;
