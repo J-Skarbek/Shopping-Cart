0bd1a650580dab70165005a7d97502f63e8f2013
J-Skarbek <john@snarkmediaagency.com>
2024-02-05 19:31:46 -0600

    Start on functions to get subtotal of cart products and submitting a cohesive object to the checkout page

--- shopping-cart-app/src/components/ShoppingCart.jsx
+++ shopping-cart-app/src/components/ShoppingCart.jsx
@@ -6,6 +6,46 @@ function ShoppingCart() {
 
   const { cartContents, emptyTheCart } = useContext(ShopContext);
 
+  const [purchaseOrder, setPurchaseOrder] = React.useState(null);
+
+  function sendToCheckout(e) {
+    e.preventDefault();
+    console.log(`Default Prevented == ${e.defaultPrevented}`);
+    let preTaxSubTotal;
+    // let purchaseTotal;
+
+    // if cartContents == empty : return $0.00
+    // if cartContents.length === 1 : purchaseTotal = Number(item.price)
+    // else for each item --> purchase total + Number(item.price)
+
+    // const purchaseOrder = cartContents.map(item => {
+    //   if (cartContents.length === 1) {
+    //     purchaseTotal = Number(item.price);
+    //   } else {
+
+    //   }
+    // })
+    const getSubTotal = () => {
+      if (cartContents.length === 0) {
+        preTaxSubTotal = Number(0.00)
+        console.log(`pretax subtotal: ${preTaxSubTotal}`)        
+      }
+      cartContents.map(item => {
+        console.log(item);
+        const quant = Number(item.quantityInCart);
+        const price = Number(item.price);
+        console.log(`Price: ${price} || quant: ${quant}`)
+        const calculate = price * quant;
+        console.log(`calculate: ${calculate}`);
+        return preTaxSubTotal = preTaxSubTotal * calculate;
+      })
+      return preTaxSubTotal;
+    }
+
+  console.log(getSubTotal());
+  return getSubTotal();
+  }
+
   const displayCartContents = cartContents.map(item => {
     return (
       <ShoppingCartCard 
@@ -30,6 +70,9 @@ function ShoppingCart() {
         </div>
         { displayCartContents.length === 0 ? <button>Shop Now</button> : displayCartContents}
         {/* {displayCartContents} */}
+      </div>
+      <div>
+        <button type="button" onClick={sendToCheckout}>Checkout</button>
       </div>
     </div>
   )
