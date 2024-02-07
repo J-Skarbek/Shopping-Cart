8ed070786b4b0d0ef8a1573e9ad47d5102b391f3
J-Skarbek <john@snarkmediaagency.com>
2024-02-07 17:45:04 -0600

    Start moving the cartMaths functions into their own util file

--- shopping-cart-app/src/components/ShoppingCart.jsx
+++ shopping-cart-app/src/components/ShoppingCart.jsx
@@ -1,59 +1,22 @@
 import React, { useContext } from "react";
 import { ShopContext } from "./RootLayout";
 import ShoppingCartCard from "./ShoppingCartCard";
+import { getSubtotal } from "../utlis/cartMaths";
 
 function ShoppingCart() {
 
   const { cartContents, emptyTheCart } = useContext(ShopContext);
 
-  const [purchaseOrder, setPurchaseOrder] = React.useState(null);
+  // const [purchaseOrder, setPurchaseOrder] = React.useState(null);
+
+  const test = cartContents;
+  console.log(test)
 
   function sendToCheckout(e) {
     e.preventDefault();
-    console.log(`Default Prevented == ${e.defaultPrevented}`);
- 
-    const roundCents = cartValue => Number.parseFloat(cartValue).toFixed(2);
-    const applyTax = cartValue => cartValue * 1.06125;
-    const calcShipping = (cartValue, shippingOption) => {
-      if (shippingOption === 'Express') {
-        return cartValue * 1.0375;
-      }
-      return cartValue * 1.01875;
-    }
-    
-    const getSubtotal = () => {
-      let preTaxSubTotal;
-      let additionArray = [];
-      if (cartContents.length === 0) {
-        preTaxSubTotal = Number(0.00);
-        console.log(`pretax subtotal: ${preTaxSubTotal}`);   
-      } else {
-        cartContents.map(item => {
-          const quant = Number(item.quantityInCart);
-          const price = Number(item.price);
-          const multiply = price * quant;
-          additionArray.push(Number(roundCents(multiply)));
-        })
-      }
-    
-      function reducer(accumulator, currentValue) {
-        const returns = accumulator + currentValue;
-        return returns;
-      }
-
-      const subTotalAll = () => {
-        if (additionArray.length === 0) {
-          console.log('There are no products in the cart.');
-          return;
-        }
-        const subTotaledValue = additionArray.reduce(reducer);
-        return subTotaledValue;
-      }
-
-      return subTotalAll();
-    }
-    console.log(`processed subtotal: ${getSubtotal()}`)
-    return getSubtotal();
+    console.log(`Default Prevented == ${e.defaultPrevented}`); 
+    console.log(`cartContentsValue: ${typeof cartContents} || testValue: ${test}`)
+    return getSubtotal(test);
   }
 
   const displayCartContents = cartContents.map(item => {
---
+++ shopping-cart-app/src/utlis/CartMaths.jsx
@@ -0,0 +1,62 @@
+import React, { useContext } from "react";
+// import { ShopContext } from "./RootLayout";
+
+export function CartMaths() {
+
+  
+
+  return (
+    <></>
+  )
+}
+
+const roundCents = cartValue => Number.parseFloat(cartValue).toFixed(2);
+
+const applyTax = cartValue => cartValue * 1.06125;
+
+const calcShipping = (cartValue, shippingOption) => {
+  if (shippingOption === 'Express') {
+    return cartValue * 1.0375;
+  }
+  return cartValue * 1.01875;
+}
+  
+export const getSubtotal = cartContents => {
+  let preTaxSubTotal;
+  let additionArray = [];
+  if (cartContents.length === 0) {
+    preTaxSubTotal = Number(0.00);
+    console.log(`pretax subtotal: ${preTaxSubTotal}`);   
+  } else {
+    cartContents.map(item => {
+      const quant = Number(item.quantityInCart);
+      const price = Number(item.price);
+      const multiply = price * quant;
+      additionArray.push(Number(roundCents(multiply)));
+    })
+  }
+
+  function reducer(accumulator, currentValue) {
+    const returns = accumulator + currentValue;
+    return returns;
+  }
+
+  const subTotalAll = () => {
+    if (additionArray.length === 0) {
+      console.log('There are no products in the cart.');
+      return;
+    }
+    const subTotaledValue = additionArray.reduce(reducer);
+    return subTotaledValue;
+  }
+  console.log(`processed subtotal: ${getSubtotal()}`)
+  return subTotalAll();
+}
+
+// export {
+//   applyTax,
+//   calcShipping,
+// }
+
+
+
