6c106599fe045acdfe1d84f7f9a0e7f49d2a4d96
J-Skarbek <john@snarkmediaagency.com>
2024-02-27 13:22:32 -0600

    Quick code cleanup

--- shopping-cart-app/src/components/HamburgerMenuExpanded.jsx
+++ shopping-cart-app/src/components/HamburgerMenuExpanded.jsx
@@ -9,41 +9,39 @@ export default function HamburgerMenuExpanded({ toggleMenu }) {
 
   return (
     <div className="modal-menu-overlay flex flex-row-reverse absolute top-0 right-0 bg-white shadow-lg">
-    <div className="hamburger-open flex flex-col bg-slate-200 w-96 h-fit p-4">
-      <div className="close-button flex justify-end">
-        <button type="button">
+      <div className="hamburger-open flex flex-col bg-slate-200 w-96 h-fit p-4">
+        <div className="close-button flex justify-end">
           <img 
             src={closeBtn} 
             alt="Close the menu."
-            onClick={toggleMenu} />
-        </button>
-      </div>
-      <div className="products-in-cart mb-4">
-        <p>Products in Cart: {countCartItems()}</p>
-      </div>
-      <div className="order-details">
-        <div className="hamburger-inner-container border-y border-t-slate-300 border-b-slate-500 py-4 my-2">
-          <div className="order-value flex justify-between mb-4">
-            <p>Order Value</p>
-            <p>$99.99(ex)</p>
-          </div>
-          <div className="shipping-value flex justify-between">
-            <p>Shipping</p>
-            <p className="uppercase">FREE</p>
+            onClick={toggleMenu} 
+          />
+        </div>
+        <div className="products-in-cart mb-4">
+          <p>Products in Cart: {countCartItems()}</p>
+        </div>
+        <div className="order-details">
+          <div className="hamburger-inner-container border-y border-t-slate-300 border-b-slate-500 py-4 my-2">
+            <div className="order-value flex justify-between mb-4">
+              <p>Order Value</p>
+              <p>$99.99(ex)</p>
+            </div>
+            <div className="shipping-value flex justify-between">
+              <p>Shipping</p>
+              <p className="uppercase">FREE</p>
+            </div>
           </div>
         </div>
+        <div className="buttons-container flex flex-col">
+          <Button 
+            text='checkout'
+          />
+          <Button 
+            text='cart'
+          />
+          <button type="buton">Cart</button>
+        </div>
       </div>
-      <div className="buttons-container flex flex-col">
-        <Button 
-          text='checkout'
-        />
-        <Button 
-          text='cart'
-        />
-        {/* <button type="button" onClick={updateMenuStatus}>Checkout</button> */}
-        <button type="buton">Cart</button>
-      </div>
-    </div>
     </div>
   )
 }
