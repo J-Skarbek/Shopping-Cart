9d995482b1e5f6af5d5efc1123c71cc6cf9a8460
J-Skarbek <john@snarkmediaagency.com>
2024-03-14 17:12:24 -0500

    Quick margin and flex adjustment

--- shopping-cart-app/src/components/MenuCartItem.jsx
+++ shopping-cart-app/src/components/MenuCartItem.jsx
@@ -4,8 +4,8 @@ import PropTypes from 'prop-types';
 export default function MenuCartItem({ productName, productImage, productPrice, productSize, quantity }) {
 
   return (
-    <div className="flex menu-mini-cart-item justify-between p-4 mb-2 border-b border-gray-300">
-      <div className="menu-cart-item-img w-1/4">
+    <div className="flex menu-mini-cart-item p-4 mb-2 border-b border-gray-300">
+      <div className="menu-cart-item-img w-1/4 mr-4">
         <img src={productImage} alt={productName} />
       </div>
       <div className="flex flex-col menu-product-details">
