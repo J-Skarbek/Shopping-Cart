6d724fcef74515ddffa558407e728e84a1168e42
J-Skarbek <john@snarkmediaagency.com>
2024-03-20 10:32:54 -0500

    More footer mobile optimization

--- shopping-cart-app/src/components/Footer.jsx
+++ shopping-cart-app/src/components/Footer.jsx
@@ -4,7 +4,6 @@ import meta from '../assets/meta.svg';
 import tikTok from '../assets/tik-tok.svg';
 import instagram from '../assets/instagram.svg';
 
-
 function Footer() {
 
   const [testValue, setTestValue] = React.useState('Magnificent Monkeys');
@@ -14,7 +13,7 @@ function Footer() {
   };
 
   return (
-    <div className="footer flex flex-col sm:flex-row items-center sm:items-end bg-slate-100 items-end py-4 mt-4">
+    <div className="footer flex flex-col sm:flex-row items-center sm:items-end bg-slate-100 items-end py-8 mt-4">
        <div className="left-col px-16 py-8 sm:py-0 flex flex-col items-center align-center sm:items-start sm:align-start">
         <img className="logo-img w-1/6 my-4" src={panda} alt="logo" />
         <h2>PANDA</h2>
@@ -27,7 +26,7 @@ function Footer() {
           ut aliquip ex ea commodo consequat.
         </p>
       </div>
-      <div className="middle-col px-16">
+      <div className="middle-col pb-8 sm:py-0 px-16">
         <p>
           This site was created as a personal project, and all images were pulled 
           from very talented photographers found on <a href="#">FashionEditorals.com</a>. 
@@ -35,14 +34,14 @@ function Footer() {
           purpose.
         </p>
       </div>
-      <div className="right-col px-16">
-        <div className="social-media-links flex">
+      <div className="right-col pb-8 sm:py-0 px-16">
+        <div className="social-media-links flex justify-center sm:justify-start">
           <img className="ig-logo w-6 mx-1" src={instagram} alt="Our Instagram" />
           <img className="tiktok-logo w-6 mx-1" src={tikTok} alt="Our Tik Tok" />
           <img className="meta-logo w-6 mx-1" src={meta} alt="Our Facebook" />
         </div>
         <div className="copyright">
-          <p>Copyright Panda 2023</p>
+          <p>Copyright Panda 2024</p>
         </div>
       </div>
     </div>
