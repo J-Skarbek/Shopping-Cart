d5eda2416acadf884d5466d939e0f75607cf28c4
J-Skarbek <john@snarkmediaagency.com>
2024-03-21 11:11:05 -0500

    Start on Checkout page component

--- shopping-cart-app/src/components/Checkout.jsx
+++ shopping-cart-app/src/components/Checkout.jsx
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { useContext } from "react";
+import { ShopContext } from "./RootLayout";
 
 export default function Checkout() {
+
+  const { cartContents, getSubtotal } = useContext(ShopContext);
+
+  console.table(cartContents);
+  console.log(getSubtotal());
 
   return (
     <div>
