8d52846a5ecd0e253870211ce2646d389bef4053
J-Skarbek <john@snarkmediaagency.com>
2024-03-28 11:31:39 -0500

    Get the cards to populate on the homepage

--- shopping-cart-app/src/App.jsx
+++ shopping-cart-app/src/App.jsx
@@ -25,6 +25,7 @@ const router = createBrowserRouter(
           index
           element={<Homepage />}
           errorElement={<Error />}
+          loader={productsLoader}
         />
         <Route 
           path='shop'
--- shopping-cart-app/src/components/HotSellers.jsx
+++ shopping-cart-app/src/components/HotSellers.jsx
@@ -1,10 +1,44 @@
 import React from "react";
+import { useLoaderData, useSearchParams } from 'react-router-dom';
+import ProductCard from "./ProductCard";
+import { nanoid } from "nanoid";
+
+export const productsLoader = async () => {
+  const res = await fetch('http://localhost:4000/allProudcts')
+  return res.json();
+}
 
 export default function HotSellers() {
 
+  const productArray = useLoaderData();
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const allProducts = [];
+
+  productArray.map(category => {
+    category.products.map(product => {
+      allProducts.push(product);
+    });
+  });
+
+  const displayHotSellers = allProducts.map(product => {
+    return (
+      <ProductCard 
+        name={product.name}
+        description={product.desciption}
+        img={product.imageArray[0]}
+        price={product.price}
+        key={nanoid()}
+        searchParams={searchParams}
+      />
+    )
+  })
+
   return(
-    <div>
-      <p>this is the hot sellers section</p>
+    <div className="w-full my-8">
+      <div className="hot-sellers w-3/4 flex">
+        { displayHotSellers }
+      </div>
     </div>
   )
 }
