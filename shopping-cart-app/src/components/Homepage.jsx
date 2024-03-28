f63834412b18d3e0ac41b37b027dc6d14ca641e4
J-Skarbek <john@snarkmediaagency.com>
2024-03-28 10:59:20 -0500

    Create a HotSellers component

--- shopping-cart-app/src/components/Homepage.jsx
+++ shopping-cart-app/src/components/Homepage.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import HeroSection from "./HeroSection";
 import CustomButton from "./customButton";
+import HotSellers from "./HotSellers";
 
 function Homepage() {
 
   return (
     <div className="homepage-body w-full">
       <HeroSection />
-      <CustomButton />
+      <HotSellers />
     </div>
   )
 }
---
+++ shopping-cart-app/src/components/HotSellers.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function HotSellers() {
+
+  return(
+    <div>
+      <p>this is the hot sellers section</p>
+    </div>
+  )
+}
